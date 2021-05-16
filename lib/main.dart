f726568efc4b5cf5afb778eb24efa9a7095ebcc4
Михаил Зотьев <mbixjkee1392@gmail.com>
2021-05-16 19:29:59 +0300

    Пример с багом при определении типа виджетов.

--- lib/main.dart
+++ lib/main.dart
@@ -19,13 +19,68 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class TestPage extends StatelessWidget {
+class TestPage extends StatefulWidget {
   const TestPage({Key key}) : super(key: key);
+
+  @override
+  _TestPageState createState() => _TestPageState();
+}
+
+class _TestPageState extends State<TestPage> {
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            AnimatedSwitcher(
+              duration: const Duration(seconds: 1),
+              // child: isLoading ? Loader() : Profile(),
+              child: isLoading ? buildLoader() : buildProfile(),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(() => isLoading = !isLoading),
+      ),
+    );
+  }
+
+  Widget buildLoader() {
+    return Icon(
+      Icons.timer,
+      size: 100,
+    );
+  }
+
+  Widget buildProfile() {
+    return Icon(
+      Icons.supervised_user_circle,
+      size: 100,
+    );
+  }
+}
+
+class Loader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Icon(
+      Icons.timer,
+      size: 100,
+    );
+  }
+}
+
+class Profile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Icon(
+      Icons.supervised_user_circle,
+      size: 100,
     );
   }
 }
